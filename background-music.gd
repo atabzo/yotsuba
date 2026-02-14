8253d5c6443c5bea84d025923d5eee09d26f40da
Bhanu <myemail.bhanu@gmail.com>
2026-02-15 01:00:14 +0530

    figured out the background-music and sfx!

---
+++ background-music.gd
@@ -0,0 +1,22 @@
+extends Node
+
+# Path to your music file
+@export var music_path: String = "res://music/1.mp3"
+
+# Reference to the AudioStreamPlayer
+var music_player: AudioStreamPlayer
+
+func _ready():
+	# Create an AudioStreamPlayer node
+	music_player = AudioStreamPlayer.new()
+	add_child(music_player)
+	
+	# Load the music file
+	var music = load(music_path)
+	music_player.stream = music
+	
+	# Enable looping
+	music_player.stream.loop = true
+	
+	# Start playing automatically
+	music_player.play()
